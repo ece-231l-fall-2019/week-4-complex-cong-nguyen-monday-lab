7d53a4191a76ee9c386ff68144e1a19f755e479d
SteelMatrix <jpschool232@gmail.com>
2019-09-17 19:26:43 -0600

    Updated runtests.cpp to test for arithmetic

--- runtests.cpp
+++ runtests.cpp
@@ -18,11 +18,21 @@ void Assert(bool cond, std::string message)
 		std::cerr << "FAIL: " << message << std::endl;
 }
 
+
 int main()
 {
 	Complex z1(4, 3);
+	Complex z2(2, 5);
+	Complex ans1(6, 8); //Addition
+	Complex ans2(2, -2); //Subtraction
+	Complex ans3(-7, 26); //Multiplication
+	Complex ans4(0.79, -0.48); //Division
 	Assert(z1.real() == 4, "real constructor");
 	Assert(z1.imag() == 3, "real constructor");
+	Assert(z1 + z2 == ans1, "real constructor");
+	Assert(z1 - z2 == ans2, "real constructor");
+	Assert(z1 * z2 == ans3, "real constructor");
+	Assert(z1 / z2 == ans4, "real constructor");
 	//Commented out for now
 	//Assert(norm(z1) == 25, "complex norm");
 
